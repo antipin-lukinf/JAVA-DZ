832b9f980333c33c5729b082c695ed1085ee2d6a
Drei4 <antipin-lukinf@mail.ru>
2022-11-27 23:45:01 +0300

    add dz2

---
+++ dz2/Mergesort.java
@@ -0,0 +1,45 @@
+package DZ.dz2;
+
+public class Mergesort {
+
+    public static void main(String[] args) {
+        int[] crs = {5, 8, 9, 10, 2, 1};
+        mergeSort(int[] src, n);
+        merge();
+
+    }
+
+
+
+    public static void mergeSort(int[] src, int n) {
+        if (n < 2) return;
+
+        int mid = n / 2;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+
+        System.arraycopy(src, 0, left, 0, mid);
+        System.arraycopy(src, mid, right, 0, n - mid);
+
+        mergeSort(left, mid);
+        
+    }
+
+    public static void merge(int[] src, int[] left, int[] right, int leftLength, int rightLength) {
+        int k = 0, i = 0, j = 0;
+
+        while (i < leftLength && j < rightLength) {
+            if (left[i] <= right[j]) src[k++] = left[i++];
+            else src[k++] = right[j++];
+        }
+
+        while (i < leftLength) {
+            src[k++] = left[i++];
+        }
+
+        while (j < rightLength) {
+            src[k++] = right[j++];
+        }
+    }
+
+}
