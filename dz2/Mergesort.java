9cdddd0567b881fe1a9123483d3a78646057c665
Drei4 <antipin-lukinf@mail.ru>
2022-11-28 13:21:14 +0300

    add dz2 merge sort

--- dz2/Mergesort.java
+++ dz2/Mergesort.java
@@ -2,44 +2,65 @@ package DZ.dz2;
 
 public class Mergesort {
 
-    public static void main(String[] args) {
-        int[] crs = {5, 8, 9, 10, 2, 1};
-        mergeSort(int[] src, n);
-        merge();
-
-    }
-
-
-
-    public static void mergeSort(int[] src, int n) {
-        if (n < 2) return;
-
-        int mid = n / 2;
-        int[] left = new int[mid];
-        int[] right = new int[n - mid];
-
-        System.arraycopy(src, 0, left, 0, mid);
-        System.arraycopy(src, mid, right, 0, n - mid);
-
-        mergeSort(left, mid);
+    public static int [] sortArray(int[] arrayA){ // сортировка Массива который передается в функцию    
+        // проверяем не нулевой ли он?
+            if (arrayA == null) {
+            return null;    }
+        // проверяем не 1 ли элемент в массиве?
+            if (arrayA.length < 2) {
+            return arrayA; 
+            }
+            // возврат в рекурсию в строки ниже см комменты.
         
+            // копируем левую часть от начала до середины
+            int [] arrayB = new int[arrayA.length / 2];
+            System.arraycopy(arrayA, 0, arrayB, 0, arrayA.length / 2);
+            // копируем правую часть от середины до конца массива, вычитаем из длины первую часть
+            int [] arrayC = new int[arrayA.length - arrayA.length / 2];
+            System.arraycopy(arrayA, arrayA.length / 2, arrayC, 0, arrayA.length - arrayA.length / 2);
+            // рекурсией закидываем поделенные обе части обратно в наш метод, он будет крутится до тех пор,
+            // пока не дойдет до 1 элемента в массиве, после чего вернется в строку и будет искать второй такой же,
+            // точнее правую часть от него и опять вернет его назад
+            arrayB = sortArray(arrayB);
+            // левая часть возврат из рекурсии строкой return arrayA;
+            arrayC = sortArray(arrayC);
+            // правая часть возврат из рекурсии строкой return arrayA;
+            // далее опять рекурсия возврата слияния двух отсортированных массивов
+            return mergeArrays(arrayB, arrayC);
+        }
+    
+    static int[] mergeArrays(int[] a1, int[] a2) {
+        int[] b = new int[a1.length + a2.length];
+        int positionA1 = 0;    int positionA2 = 0;
+        for(int i = 0; i < b.length; i++) {
+            if(positionA1 == a1.length){
+                b[i] = a2[positionA2];
+                positionA2++;
+                }
+                else if(positionA2 == a2.length){
+                b[i] = a1[positionA1];
+                positionA1++;
+                }
+                else if(a1[positionA1] < a2[positionA2]){
+                b[i] = a1[positionA1];
+                positionA1++;
+                }
+                else{
+                b[i] = a2[positionA2];
+                positionA2++;        }
+        }
+        return b;
     }
-
-    public static void merge(int[] src, int[] left, int[] right, int leftLength, int rightLength) {
-        int k = 0, i = 0, j = 0;
-
-        while (i < leftLength && j < rightLength) {
-            if (left[i] <= right[j]) src[k++] = left[i++];
-            else src[k++] = right[j++];
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[]a=new int[]{9,1,5,3,6};
+        int[]b=sortArray(a);
+        for(Integer i:b){
+            System.out.print(i);
         }
-
-        while (i < leftLength) {
-            src[k++] = left[i++];
-        }
-
-        while (j < rightLength) {
-            src[k++] = right[j++];
-        }
-    }
-
-}
+        
+}}
+   
