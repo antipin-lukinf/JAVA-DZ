69b293c14a305814e4402bf0424ef4b79588aca6
Drei4 <antipin-lukinf@mail.ru>
2022-12-02 20:50:58 +0300

    add zadach1

---
+++ DmDz/hmwork1/zadach1/src/App.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i = new Random().nextInt(-1000, 1000);
+        System.out.println(i);
+    }
+}
